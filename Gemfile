0487b67106740ce445f73bfff43f08e14a0b047e
Ariani Plukaard <ari.plukaard@gmail.com>
2022-11-15 17:03:25 +1100

    completed app

--- Gemfile
+++ Gemfile
@@ -67,3 +67,9 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+# Simple form gemfile
+gem "simple_form", github: "heartcombo/simple_form"
+
+# Faker gem
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'main'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.destroy_all
+
+4.times do
+  Flat.create!(
+    name: Faker::Address.community,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentence,
+    price_per_night: rand(75..100),
+    number_of_guests: rand(2..8),
+    img: "https://source.unsplash.com/random/1000x500/?house"
+  )
+end
